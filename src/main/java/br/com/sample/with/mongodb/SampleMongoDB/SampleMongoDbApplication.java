c02692ad157cbc666ec6bf7c6b1aabb297bd7e48
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2023-11-04 21:52:36 -0300

    Arquivos Iniciais

---
+++ src/main/java/br/com/sample/with/mongodb/SampleMongoDB/SampleMongoDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.sample.with.mongodb.SampleMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sample/with/mongodb/SampleMongoDB/SampleMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sample.with.mongodb.SampleMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
